b72f9ed29c9583c9985513103a4dce56d71b321f
Nicolas Hollmann <nicolashollmanncod3@gmail.com>
2024-05-15 10:07:59 -0300

    Create TPN42EDAFilas.java

---
+++ TPN42EDAFilas.java
@@ -0,0 +1,15 @@
+
+package tpn.pkg4.pkg2.edafilas;
+
+/**
+ *
+ * @author Hollmann
+ */
+public class TPN42EDAFilas {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
